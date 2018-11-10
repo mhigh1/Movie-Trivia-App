9b6c5a5421fd53c945bce0d85d43b1fd0a6aed85
SaiSKeyton <40468585+SaiKeyton@users.noreply.github.com>
2018-11-10 18:08:11 -0500

    Added MovieInfo Fields

--- assets/js/itunes.js
+++ assets/js/itunes.js
@@ -1,13 +1,31 @@
- const findMovie = function(event) {
+const findMovie = function(event) {
       event.preventDefault();
       const movieName = $(this).data('proj');
            $('.moviebutton').val('');
-        const queryURL = `https://itunes.apple.com/search?media=movie&entity=movie&term=${movieName}`;
+        const queryURL = `https://itunes.apple.com/search?media=movie&entity=movie&term=${movieName}&attribute=featureFilmTerm`;
           $.ajax({
           url: queryURL,
+          //dataType: "html",
           method: 'GET'
           }).then(function(response){  
-          $('#stock-view').text(response);
+            response = JSON.parse(response);
+            const movie = response.results[0];
+            //const shortDescription = movie.shortDescription;
+            const movieDescription = movie.longDescription;
+            const trackName = movie.trackName;
+            const artistName = movie.artistName;
+            const previewUrl = movie.previewUrl;
+            console.log(typeof response);
+            console.log(response);
+         // $('#stock-view').text(movieDescription);
+            const movieInfo = $("<div>");
+            movieInfo.append("<h2>"+trackName+"</h2>")
+            const trailer = $("<video>",{"src":previewUrl,"type":"video/x-m4v","controls":true});
+            
+            movieInfo.append(trailer);
+            movieInfo.append("<h3>"+"Director:  "+ artistName+"</h3>")
+            movieInfo.append("<p>"+movieDescription+"</p>")
+            $('#stock-view').html(movieInfo);
          
       })
     }
