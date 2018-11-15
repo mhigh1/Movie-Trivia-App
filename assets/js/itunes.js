6643ad7188aa89a5a42974fe60fa7a1d86a1f3bf
Keyton <213015777@logon.ds.ge.com>
2018-11-14 19:19:23 -0500

    refactor itunes api call

--- assets/js/itunes.js
+++ assets/js/itunes.js
@@ -1,33 +1,19 @@
-const findMovie = function(event) {
-      event.preventDefault();
-      const movieName = $(this).data('proj');
-           $('.moviebutton').val('');
-        const queryURL = `https://itunes.apple.com/search?media=movie&entity=movie&term=${movieName}&attribute=featureFilmTerm`;
-          $.ajax({
-          url: queryURL,
-          //dataType: "html",
-          method: 'GET'
-          }).then(function(response){  
-            response = JSON.parse(response);
-            const movie = response.results[0];
-            //const shortDescription = movie.shortDescription;
-            const movieDescription = movie.longDescription;
-            const trackName = movie.trackName;
-            const artistName = movie.artistName;
-            const previewUrl = movie.previewUrl;
-            console.log(typeof response);
-            console.log(response);
-         // $('#stock-view').text(movieDescription);
-            const movieInfo = $("<div>");
-            movieInfo.append("<h2>"+trackName+"</h2>")
-            const trailer = $("<video>",{"src":previewUrl,"type":"video/x-m4v","controls":true});
-            
-            movieInfo.append(trailer);
-            movieInfo.append("<h3>"+"Director:  "+ artistName+"</h3>")
-            movieInfo.append("<p>"+movieDescription+"</p>")
-            $('#stock-view').html(movieInfo);
-         
-      })
-    }
-    $('.moviebutton').on('click', findMovie);
-});
+const movieInfo = function (movieName) {
+  //get data from itunes api
+  const queryURL = `https://itunes.apple.com/search?media=movie&entity=movie&term=${movieName}&attribute=featureFilmTerm`;
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).then(function (response) {
+    response = JSON.parse(response);
+    const movie = response.results[0];
+    const trackName = movie.trackName;
+    const previewUrl = movie.previewUrl;
+
+    $('#modalTitle').html(`<h2>${trackName}</h2>`);
+    $('.modal-body').html(`<video src="${previewUrl}" type="video/x-m4v" controls="true" />`);
+  }); 
+}
+
+
+
